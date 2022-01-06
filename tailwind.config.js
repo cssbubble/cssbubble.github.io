6c99fc20b5ace8c5032f9a8a98623b230440c037
cssbubble <cssbubble@gmail.com>
2022-01-06 17:05:27 +0000

    social proof and single page price grid page fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
     './docs/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sp-very-dark-magenta' : 'hsl(300, 43%, 22%)',
+        'sp-soft-pink' : 'hsl(333, 80%, 67%)',
+        'sp-dark-grayish-magenta' : 'hsl(303, 10%, 53%)',
+        'sp-light-grayish-magenta' : 'hsl(300, 24%, 96%)',
+        
+        'sppg-customCyan' : 'hsl(179, 62%, 43%)',
+        'sppg-brightYellow': 'hsl(71, 73%, 54%)',
+        'sppg-lightGray': 'hsl(204, 43%, 93%)',
+        'sppg-grayishBlue': 'hsl(218, 22%, 67%)',
+      },
+      maxWidth: {
+          's' : '375px',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
