be28f5512292d258a53df676bceab7e648d193ce
cssbubble <cssbubble@gmail.com>
2022-01-07 17:44:12 +0000

    sunnyside and ecommerce page minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,13 @@ module.exports = {
         'sppg-grayishBlue': 'hsl(218, 22%, 67%)',
 
         'pcc-bg-color': 'hsl(185, 75%, 39%)',
+
+        'ec-customOrange': 'hsl(26, 100%, 55%)',
+          'ec-paleOrange': 'hsl(25, 100%, 94%)',
+          'ec-veryDarkBlue': 'hsl(220, 13%, 13%)',
+          'ec-darkGrayishBlue': 'hsl(219, 9%, 45%)',
+          'ec-grayishBlue': 'hsl(220, 14%, 75%)',
+          'ec-lightGrayishBlue': 'hsl(223, 64%, 98%)',
       },
       maxWidth: {
           's' : '375px',
