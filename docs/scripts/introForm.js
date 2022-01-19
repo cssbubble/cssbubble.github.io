58f8cf76566b8a28d62028362c9967c1326a7498
cssbubble <cssbubble@gmail.com>
2022-01-19 12:04:50 +0000

    intro signup form page updated

--- docs/scripts/introForm.js
+++ docs/scripts/introForm.js
@@ -1,9 +1,12 @@
 const form = document.getElementById('form');
 const firstName = document.getElementById('fName');
+const firstError = document.getElementById('first-name-error')
 const lastName = document.getElementById('lName');
+const lastError = document.getElementById('last-name-error')
 const email = document.getElementById('email');
+const emailError = document.getElementById('email-error')
 const password = document.getElementById('password');
-
+const passwordError = document.getElementById('password-error')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
@@ -18,62 +21,91 @@ function checkInputs(){
     const pValue = password.value.trim(); 
 
     if(fValue === ''){
-        setErrorFor(firstName, 'First name can not be empty')
+        firstName.classList.add('error');
+        firstError.classList.remove('hidden')
+        firstError.classList.add('error-msg');
     } else {
-        setSuccessFor(firstName);
-    } 
-
-    if(lValue === ''){
-        setErrorFor(lastName, 'Last name can not be empty')
-    } else {
-        setSuccessFor(lastName);
+        firstName.classList.add('success');
+        firstError.classList.add('hidden');
     }
 
-    if(!isEmail (eValue)){
-        setErrorFor(email, 'Looks like this is not an email')
+    if(lValue === ''){
+        lastName.classList.add('error');
+        lastError.classList.remove('hidden')
+        lastError.classList.add('error-msg');
     } else {
-        setSuccessFor(email);
+        lastName.classList.add('success');
+        lastError.classList.add('hidden')
+    }
+
+    if(eValue === ''){
+        email.classList.add('error');
+        emailError.classList.remove('hidden')
+        emailError.classList.add('error-msg');
+    }else {
+        email.classList.add('success');
+        emailError.classList.add('hidden')
     }
 
     if(pValue === ''){
-        setErrorFor(password, 'Password can not be empty')
-    } else {
-        setSuccessFor(password);
+        password.classList.add('error');
+        passwordError.classList.remove('hidden')
+        passwordError.classList.add('error-msg');
+    }else {
+        password.classList.add('success');
+        passwordError.classList.add('hidden')
+        password.classList.remove('error');
     }
 
+    // if(fValue === ''){
+    //     setErrorFor(firstName, 'First name can not be empty')
+    // } else {
+    //     setSuccessFor(firstName);
+    // } 
+
+    // if(lValue === ''){
+    //     setErrorFor(lastName, 'Last name can not be empty')
+    // } else {
+    //     setSuccessFor(lastName);
+    // }
+
+    // if(!isEmail (eValue)){
+    //     setErrorFor(email, 'Looks like this is not an email')
+    // } else {
+    //     setSuccessFor(email);
+    // }
+
+    // if(pValue === ''){
+    //     setErrorFor(password, 'Password can not be empty')
+    // } else {
+    //     setSuccessFor(password);
+    // }
+
 }
 
-function setErrorFor(input, message){
-    const formControl = input.parentElement; 
-    const info = formControl.querySelector('p');
-    info.innerText = message;
-    const img = formControl.querySelector('img');
+// function setErrorFor(input, message){
+//     const formControl = input.parentElement; 
+//     const info = formControl.querySelector('p');
+//     info.innerText = message;
+//     const img = formControl.querySelector('img');
 
-    input.className = 'w-full border-red-500 p-4 rounded-lg shadow-sm focus:outline-none focus:accent-intro-form-accent focus:ring-1 focus:ring-intro-form-accent';
-    img.className = 'absolute right-2 top-4';
-    info.className = 'text-xs text-intro-form-red text-right mt-2 italic'
-}
+//     input.className = 'w-full border-red-500 p-4 rounded-lg shadow-sm focus:outline-none focus:accent-intro-form-accent focus:ring-1 focus:ring-intro-form-accent';
+//     // img.className = 'absolute right-2 top-4 bg-no-repeat';
+//     info.className = 'text-xs text-intro-form-red text-right mt-2 italic'
+// }
 
-function setSuccessFor(input){ 
-    const formControl = input.parentElement; 
-    const info = formControl.querySelector('p');
-    // info.innerText = message;
-    const img = formControl.querySelector('img');
+// function setSuccessFor(input){ 
+//     const formControl = input.parentElement; 
+//     const info = formControl.querySelector('p');
+//     info.innerText = message;
+//     const img = formControl.querySelector('img');
 
-    input.className ='w-full border-green-500 border-2 p-4 rounded-lg shadow-sm focus:outline-none focus:accent-intro-form-accent focus:ring-1 focus:ring-intro-form-accent';
-    img.className = 'hidden';
-    info.className = 'hidden';   
-}
+//     input.className ='w-full border-green-500 border-2 p-4 rounded-lg shadow-sm focus:outline-none focus:accent-intro-form-accent focus:ring-1 focus:ring-intro-form-accent';
+//     img.className = 'hidden';
+//     info.className = 'hidden';   
+// }
 
 function isEmail (email){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
-function success(first, last, mail, password){
-    if( first.className || last.className || mail.className || password.className === 'w-full border-green-500 border-2 p-4 rounded-lg shadow-sm focus:outline-none focus:accent-intro-form-accent focus:ring-1 focus:ring-intro-form-accent') {
-        alert('form submitted successfully');
-        first.value = null;
-        last.value = null;
-        mail.value = null; 
-        password.value = null;   }
-}
